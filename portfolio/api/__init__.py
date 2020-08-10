e57402d111a724a477b619997ae30bc77fdf06dd
Sean Jones <sean.d.jones92@gmail.com>
2020-08-10 06:52:47 -0600

    add mockme api

--- portfolio/api/__init__.py
+++ portfolio/api/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-from flask import (Blueprint, Response, current_app)
+from flask import (Blueprint, Response, current_app, request)
+import random
 
 app = current_app
 siteapi = Blueprint('siteapi', __name__, url_prefix='/api')
@@ -18,3 +19,19 @@ def resume():
     with app.open_resource('static/resume.json') as resumefile:
         resume = resumefile.read()
     return Response(resume, status=200)
+
+
+# curl -X POST localhost:5000/api/mockme -d '{"message": "why would you do this?"}' -H 'Content-Type: application/json'
+@siteapi.route('/mockme', methods=['POST'])
+def mockme():
+    output_text = ""
+    input_text = str(request.json.get('message'))
+    for char in input_text:
+        if char.isalpha():
+            if random.random() > 0.5:
+                output_text += char.upper()
+            else:
+                output_text += char.lower()
+        else:
+            output_text += char
+    return output_text
