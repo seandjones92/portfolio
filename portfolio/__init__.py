2b69f273c6a6c854c7bdce6c1d78099847437b07
Sean Jones <sean.d.jones92@gmail.com>
2020-08-10 07:56:51 -0600

    getting swagger ui set up

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+from flask_swagger_ui import get_swaggerui_blueprint
 from portfolio import api, gui
 import logging
+
+SWAGGER_URL = '/api-docs'
+API_URL = '/static/assets/json/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(SWAGGER_URL,
+                                              API_URL,
+                                              config={'app_name': 'My APIs'})
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.register_blueprint(api.siteapi)
     app.register_blueprint(gui.sitegui)
+    app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 
     return app
 
