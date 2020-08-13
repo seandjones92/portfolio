66652c2834f449d7e11ef01d5092870fe965c836
Sean Jones <17571045+seandjones92@users.noreply.github.com>
2020-08-12 19:43:33 -0600

    Httpsredirect (#5)
    * added prototype redirect
    
    * imported redirect
    
    * finished httpredirect, need to merge to master branch to test

--- portfolio/api/__init__.py
+++ portfolio/api/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import (Blueprint, Response, current_app, request)
+from flask import (Blueprint, Response, current_app, request, redirect)
 import random
 
 app = current_app
@@ -23,17 +23,19 @@ def resume():
 
 @siteapi.route('/mockme', methods=['POST'])
 def mockme():
-    output_text = ""
-    input_text = str(request.json.get('message'))
-    for char in input_text:
-        if char.isalpha():
-            if random.random() > 0.5:
-                output_text += char.upper()
+    response = handle_proxy_http_to_https()
+    if response is not None:
+        output_text = ""
+        input_text = str(request.json.get('message'))
+        for char in input_text:
+            if char.isalpha():
+                if random.random() > 0.5:
+                    output_text += char.upper()
+                else:
+                    output_text += char.lower()
             else:
-                output_text += char.lower()
-        else:
-            output_text += char
-    return output_text
+                output_text += char
+                return output_text
 
 
 @siteapi.route('/roll', methods=['POST'])
@@ -67,3 +69,17 @@ def emacsinstaller():
                 'static/assets/scripts/emacspublic.sh') as emacsfile:
             emacsscript = emacsfile.read()
     return Response(emacsscript, status=200)
+
+
+def handle_proxy_http_to_https():
+    host = request.host
+    if ':' in host:
+        host = host.split(':')[0]
+    if host != 'localhost':
+        for i in request.headers:
+            if i[0].lower() == 'x-forwarded-proto':
+                if i[1].lower() == 'http':
+                    url = 'https://' + host + request.path
+                    response = redirect(url, 308)
+                    return response
+    return None
