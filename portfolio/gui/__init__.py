9b470e4c9bd3b223b394b67a39f55bd0b615bfea
Sean Jones <sean.d.jones92@gmail.com>
2020-08-15 10:22:43 -0600

    taking down main page, prepping for site reworking

--- portfolio/gui/__init__.py
+++ portfolio/gui/__init__.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
 
-import flask
-from flask import (Blueprint, render_template)
-import requests
+from flask import Blueprint
 
 sitegui = Blueprint('sitegui', __name__)
 
 
 @sitegui.route('/')
 def homepage():
-    resumeapi = flask.request.host_url + "/api/resume"
-    resumejs = requests.get(resumeapi)
-    return render_template('index.html', resume=resumejs.json())
+    return "Working on it..."
